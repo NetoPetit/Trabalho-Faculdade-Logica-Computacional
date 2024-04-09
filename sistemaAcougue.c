c44fe405235d8a522c58ecf760d7cceb494dd5fe
NetoPetit <cidao_petit5@hotmail.com>
2024-04-09 01:16:01 -0300

    atualização sistema

--- sistemaAcougue.c
+++ sistemaAcougue.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>
+//#include <locale.h>
 
 int main(void)
 {
     int tipoCarne;
     float estoque, quilos, custoProduto, vendaProduto, valorVenda, imposto, valorLiquido,
     lucro, estoqueAtual, valorCusto;
-
-    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    //setlocale(LC_ALL, "Portuguese_Brazil");
     
     printf("======================================================\n");
-    printf("||SISTEMA DE CONTROLE DE VENDAS E ESTOQUE DE AÇOUGUE|| \n");
+    printf("||SISTEMA DE CONTROLE DE VENDAS E ESTOQUE DE ACOUGUE|| \n");
     printf("======================================================\n");
     printf("\n");
-    printf("TIPOS DE CARNES: \n\n 1 - Bovina \n 2 - Suína \n 3 - Ave \n");
+    printf("TIPOS DE CARNES: \n\n 1 - Bovina \n 2 - Suina \n 3 - Ave \n");
     printf("\n");
-    printf("Escolha o número do tipo de carne que deseja consultar: ");
+    printf("Escolha o numero do tipo de carne que deseja consultar: ");
     scanf("%d", &tipoCarne);
     printf("\n");
     
         if (tipoCarne == 1)
         {
             printf("==============================\n");
-            printf("||VOCÊ ESCOLHEU CARNE BOVINA||\n");
+            printf("||VOCE ESCOLHEU CARNE BOVINA||\n");
             printf("==============================\n");
             printf("\n");
             printf("Quantos quilos o seu estoque de carne Bovina tinha ?\n");
             scanf("%f", &estoque);
-            printf("Quantos quilos de carne Bovina você vendeu ?\n");
+            printf("Quantos quilos de carne Bovina voce vendeu ?\n");
             scanf("%f", &quilos);
 
-                if (quilos > estoque)//se inserir mais quilos que o disponível no estoque o sistema se encerra
+                if (quilos > estoque)//se inserir mais quilos que o disponivel no estoque o sistema se encerra
                 {
                     printf("\n");
-                    printf("Valor inválido! Reinicie o Sistema!\n");
+                    printf("Operacao invalida! Valor maior que o estoque! Reinicie o sitema!\n");
                     return 0;
                 }
 
@@ -55,34 +56,34 @@ int main(void)
             lucro = (valorLiquido - valorCusto);
             estoqueAtual = (estoque - quilos);
 
-            printf("Seu imposto é de 18,00%%\n");
+            printf("Seu imposto e de 18,00%%\n");
             printf("Seu valor de custo foi R$%2.2f\n", valorCusto);
             printf("O valor da sua venda bruta foi de R$%2.2f\n", valorVenda);
-            printf("Você pagou de imposto R$%2.2f\n", imposto);
-            printf("Valor da receita líquida com descontos R$%2.2f\n", valorLiquido);
-            printf("Seu lucro é de R$%2.2f\n", lucro);
+            printf("Voce pagou de imposto R$%2.2f\n", imposto);
+            printf("Valor da receita liquida com descontos R$%2.2f\n", valorLiquido);
+            printf("Seu lucro e de R$%2.2f\n", lucro);
             printf("Seu estoque era de %2.2fKG\n", estoque);
-            printf("Você retirou do seu estoque %2.2fKG\n", quilos);
-            printf("Seu estoque atual é: %2.2fKG\n", estoqueAtual);
+            printf("Voce retirou do seu estoque %2.2fKG\n", quilos);
+            printf("Seu estoque atual e: %2.2fKG\n", estoqueAtual);
             printf("\n");
-            //qual a taxa de imposto a ser paga? perguntar pro usuário?
+            //qual a taxa de imposto a ser paga? perguntar pro usuario?
         }
 
         if (tipoCarne == 2)
         {
             printf("=============================\n");
-            printf("||VOCÊ ESCOLHEU CARNE SUÍNA||\n");
+            printf("||VOCE ESCOLHEU CARNE SUINA||\n");
             printf("=============================\n");
             printf("\n");
-            printf("Quantos quilos o seu estoque de carne Suína tinha ?\n");
+            printf("Quantos quilos o seu estoque de carne Suina tinha ?\n");
             scanf("%f", &estoque);
-            printf("Quantos quilos de carne Suína você vendeu ?\n");
+            printf("Quantos quilos de carne Suina voce vendeu ?\n");
             scanf("%f", &quilos);
 
-                if (quilos > estoque)//se inserir mais quilos que o disponível no estoque o sistema se encerra
+                if (quilos > estoque)//se inserir mais quilos que o disponivel no estoque o sistema se encerra
                 {
                     printf("\n");
-                    printf("Valor inválido! Reinicie o Sistema!\n");
+                    printf("Operacao invalida! Valor maior que o estoque! Reinicie o sitema!\n");
                     return 0;
                 }
 
@@ -104,34 +105,34 @@ int main(void)
             lucro = (valorLiquido - valorCusto);
             estoqueAtual = (estoque - quilos);
 
-            printf("Seu imposto é de 14,00%%\n");
+            printf("Seu imposto e de 14,00%%\n");
             printf("Seu valor de custo foi R$%2.2f\n", valorCusto);
             printf("O valor da sua venda bruta foi de R$%2.2f\n", valorVenda);
-            printf("Você pagou de imposto R$%2.2f\n", imposto);
-            printf("Valor da receita líquida com descontos R$%2.2f\n", valorLiquido);
-            printf("Seu lucro é de R$%2.2f\n", lucro);
+            printf("Voce pagou de imposto R$%2.2f\n", imposto);
+            printf("Valor da receita liquida com descontos R$%2.2f\n", valorLiquido);
+            printf("Seu lucro e de R$%2.2f\n", lucro);
             printf("Seu estoque era de %2.2fKG\n", estoque);
-            printf("Você retirou do seu estoque %2.2fKG\n", quilos);
-            printf("Seu estoque atual é: %2.2fKG\n", estoqueAtual);
+            printf("Voce retirou do seu estoque %2.2fKG\n", quilos);
+            printf("Seu estoque atual e: %2.2fKG\n", estoqueAtual);
             printf("\n");
-            //qual a taxa de imposto a ser paga? perguntar pro usuário?
+            //qual a taxa de imposto a ser paga? perguntar pro usuario?
         }
 
         if (tipoCarne == 3)
         {
             printf("==============================\n");
-            printf("||VOCÊ ESCOLHEU CARNE DE AVE||\n");
+            printf("||VOCE ESCOLHEU CARNE DE AVE||\n");
             printf("==============================\n");
             printf("\n");
             printf("Quantos quilos o seu estoque de carne de Ave tinha ?\n");
             scanf("%f", &estoque);
-            printf("Quantos quilos de carne de Ave você vendeu ?\n");
+            printf("Quantos quilos de carne de Ave voce vendeu ?\n");
             scanf("%f", &quilos);
             
-                if (quilos > estoque)//se inserir mais quilos que o disponível no estoque o sistema se encerra
+                if (quilos > estoque)//se inserir mais quilos que o disponivel no estoque o sistema se encerra
                 {
                     printf("\n");
-                    printf("Valor inválido! Reinicie o Sistema!\n");
+                    printf("Operacao invalida! Valor maior que o estoque! Reinicie o sitema!\n");
                     return 0;
                 }
 
@@ -153,26 +154,28 @@ int main(void)
             lucro = (valorLiquido - valorCusto);
             estoqueAtual = (estoque - quilos);
 
-            printf("Seu imposto é de 9,00%%\n");
+            printf("Seu imposto e de 9,00%%\n");
             printf("Seu valor de custo foi R$%2.2f\n", valorCusto);
             printf("O valor da sua venda bruta foi de R$%2.2f\n", valorVenda);
-            printf("Você pagou de imposto R$%2.2f\n", imposto);
-            printf("Valor da receita líquida com descontos R$%2.2f\n", valorLiquido);
-            printf("Seu lucro é de R$%2.2f\n", lucro);
+            printf("Voce pagou de imposto R$%2.2f\n", imposto);
+            printf("Valor da receita liquida com descontos R$%2.2f\n", valorLiquido);
+            printf("Seu lucro e de R$%2.2f\n", lucro);
             printf("Seu estoque era de %2.2fKG\n", estoque);
-            printf("Você retirou do seu estoque %2.2fKG\n", quilos);
-            printf("Seu estoque atual é: %2.2fKG\n", estoqueAtual);
+            printf("Voce retirou do seu estoque %2.2fKG\n", quilos);
+            printf("Seu estoque atual e: %2.2fKG\n", estoqueAtual);
             printf("\n");
-            //qual a taxa de imposto a ser paga? perguntar pro usuário?
+            //qual a taxa de imposto a ser paga? perguntar pro usuario?
         }
 
     else if (tipoCarne < 1 || tipoCarne > 3)
     {
-        printf("Opção indisponível! Reinicie o sitema!\n");
+        printf("Codigo inexistente! Reinicie o sitema!\n");
         printf("\n");
     }
 
     printf("======================\n");
     printf("||FIM DA CONSULTA!!!||\n");
     printf("======================\n");
+
+    system("PAUSE");
 }
